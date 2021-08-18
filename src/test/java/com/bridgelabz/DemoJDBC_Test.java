ba836c9639a18dd7d6ceb509f3843621ea467f05
Ganesh Gavhad <ganeshgavhad9@gmail.com>
2021-08-18 20:16:03 +0530

    Added : UC1 Connect To Database

--- src/test/java/com/bridgelabz/DemoJDBC_Test.java
+++ src/test/java/com/bridgelabz/DemoJDBC_Test.java
@@ -1,3 +1,13 @@
+/********************************************************************
+ *Purpose : Test Cases for Java Database connectivity Program using 7 steps
+ *        1.Import java.sql*   2.Load and Register driver
+ *        3. Create connection 4. Create Statement
+ *        5. Execute Query     6. Process The Results
+ *        7. close connection.
+ * @Auther : Ganesh Gavhad
+ * @since  : 15-08-2021
+ * @version : 1.0
+ **********************************************************************/
 
 package com.bridgelabz;
 
